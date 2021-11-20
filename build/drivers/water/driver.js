e5ba29b8311bec0d46847d55f1f8acba6591035b
JirLap <j.lapackovi@gmail.com>
2021-11-20 20:24:47 +0100

    Visible last values

--- build/drivers/water/driver.js
+++ build/drivers/water/driver.js
@@ -22,10 +22,10 @@ along with com.gruijter.powerhour.  If not, see <http://www.gnu.org/licenses/>.s
 const GenericDriver = require('../generic_driver.js');
 
 const driverSpecifics = {
-	driverId: 'power',
-	originDeviceCapabilities: ['measure_power', 'meter_power', 'meter_power.peak', 'meter_power.offPeak'],
-	deviceCapabilities: ['meter_kwh_this_hour', 'meter_kwh_last_hour', 'meter_kwh_this_day', 'meter_kwh_last_day',
-		'meter_kwh_this_month', 'meter_kwh_last_month', 'meter_kwh_this_year', 'meter_kwh_last_year'],
+	driverId: 'water',
+	originDeviceCapabilities: ['meter_water'],
+	deviceCapabilities: ['meter_m3_this_hour', 'meter_m3_last_hour', 'meter_m3_this_day', 'meter_m3_last_day',
+		'meter_m3_this_month', 'meter_m3_last_month', 'meter_m3_this_year', 'meter_m3_last_year'],
 };
 
 class sumDriver extends GenericDriver {
